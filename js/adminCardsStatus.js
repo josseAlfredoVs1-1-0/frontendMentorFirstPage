64477af82a4e902ccdcf2307af70b3b0596b976f
JosseAlfredo110 <jossealfredolap@gmail.com>
2026-08-03 09:07:50 -0600

    generator cards - editing feaatures

--- js/adminCardsStatus.js
+++ js/adminCardsStatus.js
@@ -1,13 +1,13 @@
 import { darkAll } from './applyDarkInitial.js';
 import './filterByCategory.js';
 import { cardsStatus } from './toggleCardsSwtch.js';
-
+import {genC} from './renderCardsSwtch.js';
 
 if (typeof document !== "undefined" &&
     typeof window !== "undefined") {
     document.addEventListener("DOMContentLoaded", function () {
 
-
+        genC();
         darkAll();
         cardsStatus();
         //chargeState();
---
+++ js/renderCardsSwtch.js
@@ -0,0 +1,60 @@
+import data from '../json/data.json' with { type: 'json'}
+
+console.log("data", data)
+
+export function genC() {
+    data.forEach((e) => {
+        let nb = card(e.logo, e.name, e.description, e.isActive)
+    })
+}
+
+let tagmaincontainer = document.createElement('main')
+tagmaincontainer.setAttribute('id', 'containerC')
+document.body.querySelector('#sectionMainContainer').append(tagmaincontainer)
+
+let c = (e) => document.createElement(`${e}`)
+
+function card(l, n, d, a) {
+
+    let article = c('article')
+    article.classList.add("boxFigureContainer")
+
+    /* HEAD*/
+    let head = c('header')
+    let asidel = c('aside')
+    let asider = c('aside')
+    let ico = c('img')
+    ico.src = l
+    let tit = c('h1')
+    tit.textContent = n
+    let desc = c('p')
+    desc.textContent = d
+    asidel.append(ico)
+    asider.append(tit, desc)
+    head.append(asidel, asider)
+    /* class - id - etc */
+
+
+    /* FOOTER */
+    let foot = c('footer')
+    let footasidel = c('aside')
+    let footasider = c('aside')
+
+    let swch = c('button')
+    let mark = c('div')
+    mark.classList.add("testbutt")
+    let rem = c('button')
+
+    swch.classList.add(a)
+    rem.textContent = "Remove"
+    console.log("class swtch", swch.classList)
+
+    swch.append(mark)
+    footasidel.append(rem)
+    footasider.append(swch)
+    foot.append(footasidel, footasider)
+
+    article.append(head, foot)
+
+    document.body.querySelector('#containerC').append(article)
+}
